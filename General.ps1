eaa7caa9d4a5967dd6d5e0c3db3645030cc2e81c
Adiv <adiv.abramson@outlook.com>
2024-05-01 16:10:43 -0500

    Add files via upload

---
+++ General.ps1
@@ -0,0 +1,16 @@
+﻿<#
+//Create backup folder
+$today = get-date -Format MM-dd-yyyy
+$path = "<path>\" + $today
+mkdir $path
+
+
+# Update ScratchPad.txt with current date
+$path = "<path>ScratchPad.txt"
+$date = Get-Date -Format "MM/dd/yyyy"
+$regex = "\d{2}\/\d{2}\/\d{4}"
+(Get-Content -path $path -Raw) -replace $regex, $date | Set-Content -path $path
+Start-Process -FilePath $path
+
+
+#>
